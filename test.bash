30efd0b70911ab421a55d1ac40794d324ebdea1e
Kanta Hirazawa <s24C1101LD@s.chibakoudai.jp>
2025-12-25 23:07:52 +0900

    edit

--- test.bash
+++ test.bash
@@ -2,33 +2,24 @@
 # SPDX-FileCopyrightText: 2025 Kanta Hirazawa
 # SPDX-License-Identifier: BSD-3-Clause
 
-set -e
-set -x
+ng () {
+	echo ${1}行目が違うよ
+	res=1
+}
+
+res=0
 
 out=$(echo 2025 | ./year_conv)
-if [ "${out}" = "令和7年" ]; then
-    echo "Test 1 OK"
-else
-    echo "Test 1 Failed: ${out}"
-    exit 1
-fi
+[ "${out}" = "令和7年" ] || ng "$LINENO"
 
 out=$(echo s64 | ./year_conv)
-if [ "${out}" = "1989" ]; then
-    echo "Test 2 OK"
-else
-    echo "Test 2 Failed: ${out}"
-    exit 1
-fi
+[ "${out}" = "1989" ] || ng "$LINENO"
 
-echo "test" | ./year_conv 2> err.log || true
+out=$(echo あ | ./year_conv 2> /tmp/err)
+[ "$?" = 1 ]      || ng "$LINENO"
+[ "${out}" = "" ] || ng "$LINENO"
+grep -q "計算不可" /tmp/err || ng "$LINENO"
 
-if grep -q "計算不可" err.log; then
-    echo "Test 3 OK"
-else
-    echo "Test 3 Failed"
-    exit 1
-fi
-
-rm -f err.log
-exit 0
+[ "$res" = 0 ] && echo OK
+rm -f /tmp/err
+exit $res
